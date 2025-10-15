7baacecfeb1d708c83569542e83bbbd4a979e481
Rishikarpe <karperishabh@gmail.com>
2025-10-16 01:56:51 +0530

    testing

--- 5. test.py
+++ 5. test.py
@@ -3,7 +3,6 @@ import joblib
 model = joblib.load("sentiment_model.pkl")
 vectorizer = joblib.load("tfidf_vectorizer.pkl")
 
-# Try predicting on new text
 sample = input("Enter your review: ")
 sample_tfidf = vectorizer.transform([sample])
 prediction = model.predict(sample_tfidf)
